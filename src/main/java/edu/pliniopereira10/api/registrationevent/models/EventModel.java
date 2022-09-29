71dce4011a9ad9c6cc2279a702071c541286d37f
plinio.pereira10 <plinio.pereira10@gmail.com>
2022-09-29 16:37:57 -0300

    If applied, this commit will update the name class 'EventModel'

--- src/main/java/edu/pliniopereira10/api/registrationevent/models/EventModel.java
+++ src/main/java/edu/pliniopereira10/api/registrationevent/models/EventModel.java
@@ -1,14 +1,46 @@
 package edu.pliniopereira10.api.registrationevent.models;
 
-public class EventModel {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_REGISTRATION_EVENTS")
+public class EventModel implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false)
 	private String name;
+
+	@Column(nullable = false)
 	private String location;
+
+	@Column(nullable = false)
 	private String date;
+
+	@Column(nullable = false)
 	private String time;
 
 	public EventModel() {
 
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getName() {
