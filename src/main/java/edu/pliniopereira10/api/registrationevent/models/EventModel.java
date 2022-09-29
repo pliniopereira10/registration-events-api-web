4f4de92765679094404a36f27652805e6dd9123c
plinio.pereira10 <plinio.pereira10@gmail.com>
2022-09-29 14:40:05 -0300

    If applied, this commit will create the name class 'EventModel'

---
+++ src/main/java/edu/pliniopereira10/api/registrationevent/models/EventModel.java
@@ -0,0 +1,46 @@
+package edu.pliniopereira10.api.registrationevent.models;
+
+public class EventModel {
+
+	private String name;
+	private String location;
+	private String date;
+	private String time;
+
+	public EventModel() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
