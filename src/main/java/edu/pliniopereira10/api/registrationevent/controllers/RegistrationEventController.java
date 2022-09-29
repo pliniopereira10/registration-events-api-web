c1e12fbef5a351accd9fd2cc219898c01a0a7cad
plinio.pereira10 <plinio.pereira10@gmail.com>
2022-09-29 17:45:44 -0300

    If applied, this commit will update class named 'Registration Event Controller' and create method get and post 'form'

--- src/main/java/edu/pliniopereira10/api/registrationevent/controllers/RegistrationEventController.java
+++ src/main/java/edu/pliniopereira10/api/registrationevent/controllers/RegistrationEventController.java
@@ -1,19 +1,35 @@
 package edu.pliniopereira10.api.registrationevent.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.pliniopereira10.api.registrationevent.models.EventModel;
+import edu.pliniopereira10.api.registrationevent.services.EventService;
 
 @Controller
 public class RegistrationEventController {
+
+	@Autowired
+	private EventService eventService;
 
 	@RequestMapping("/")
 	public String home() {
 		return "home";
 	}
 
-	@RequestMapping("/registration-events")
+	@GetMapping("/registration-events")
 	public String form() {
 		return "event/formEvent";
+	}
+
+	@PostMapping("/registration-events")
+	public String form(EventModel eventModel) {
+		eventService.save(eventModel);
+
+		return "redirect:registration-events";
 	}
 
 }
