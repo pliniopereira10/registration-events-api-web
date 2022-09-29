cb1f1c35aa3fed99d0ba6be9c2978ac5438e5053
plinio.pereira10 <plinio.pereira10@gmail.com>
2022-09-29 17:40:53 -0300

    If applied, this commit will create class service named 'EventService'

---
+++ src/main/java/edu/pliniopereira10/api/registrationevent/services/EventService.java
@@ -0,0 +1,28 @@
+package edu.pliniopereira10.api.registrationevent.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.pliniopereira10.api.registrationevent.models.EventModel;
+import edu.pliniopereira10.api.registrationevent.repositories.EventRepository;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository eventRepository;
+
+	@Transactional
+	public Object save(EventModel eventModel) {
+		return eventRepository.save(eventModel);
+	}
+
+	public List<EventModel> findAll() {
+		return eventRepository.findAll();
+	}
+
+}
