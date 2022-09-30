141c374fa67f8e227b57630afbecd5e999254250
plinio.pereira10 <plinio.pereira10@gmail.com>
2022-09-30 11:41:46 -0300

    If applied, this commit will create the method @GetMapping 'eventDetail' in the class named 'RegistrationEventController'

--- src/main/java/edu/pliniopereira10/api/registrationevent/controllers/RegistrationEventController.java
+++ src/main/java/edu/pliniopereira10/api/registrationevent/controllers/RegistrationEventController.java
@@ -1,10 +1,12 @@
 package edu.pliniopereira10.api.registrationevent.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -40,6 +42,15 @@ public class RegistrationEventController {
 		ModelAndView modelAndView = new ModelAndView("/event/listEvents");
 		List<EventModel> events = eventService.findAll();
 		modelAndView.addObject("events", events);
+
+		return modelAndView;
+	}
+
+	@GetMapping("/list-events/{id}")
+	public ModelAndView eventDetail(@PathVariable("id") long id) {
+		Optional<EventModel> eventModelOptional = eventService.findById(id);
+		ModelAndView modelAndView = new ModelAndView("/event/eventsDetails");
+		modelAndView.addObject("eventDetail", eventModelOptional);
 
 		return modelAndView;
 	}
